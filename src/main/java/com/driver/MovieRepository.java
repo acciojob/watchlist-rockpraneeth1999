d49157f98e81099c1e2354849f053c2973e60019
Rock <rockpraneeth1999@gmail.com>
2023-08-19 18:04:03 +0530

    Third commit

--- src/main/java/com/driver/MovieRepository.java
+++ src/main/java/com/driver/MovieRepository.java
@@ -1,9 +1,12 @@
 package com.driver;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+@Repository
 public class MovieRepository {
 
     HashMap<String,Movie> moviesDB=new HashMap<>();
--- src/main/java/com/driver/MovieService.java
+++ src/main/java/com/driver/MovieService.java
@@ -1,9 +1,11 @@
 package com.driver;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class MovieService {
 
     @Autowired
